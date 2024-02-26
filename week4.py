83c4e34b0e9b27b84c3836dacb074723c4e90783
Nidhin Naijo <100615229+Nidhin-N@users.noreply.github.com>
2024-02-26 10:06:56 +0000

    Update week4.py

--- week4.py
+++ week4.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def next_states(s):
     allString = []
     if s[-1] == "I":
@@ -14,16 +17,12 @@ def next_states(s):
 
 
 def breadthfirstdictionarysearch(p):
-    agenda = [["MI"]]
-    visited = {"MI":["MII","MIU"]}
+    agenda = ["MI"]
+    visited = {"MI":None}
     agendaMaxLen = 1
     extendCount = 0
     while agenda:
-        path = agenda.pop(0)
-        currentState = path[-1]
-        print("Current state:", currentState)
-        print(visited)
-        print("Agenda",agenda)
+        currentState = agenda.pop(0)
         if currentState == p:
             path = [currentState]
             while visited[currentState] is not None:
@@ -34,15 +33,11 @@ def breadthfirstdictionarysearch(p):
         if extendCount >= 5000:
             return [0,0,0]
         nextStates = next_states(currentState)
-        print("current state",currentState)
-        print("Next states:", nextStates)
         for nextState in nextStates:
             if nextState not in visited:
-                state = next_states(nextState)
-                visited[nextState] = state
-                agenda.extend(state)
-                agendaMaxLen = max(agendaMaxLen, len(agenda))
-                print("Added to agenda:", nextState)
+                visited[nextState] = currentState
+                agenda.append(nextState)
+        agendaMaxLen = max(agendaMaxLen, len(agenda))
         extendCount += 1
     return [0,0,0]
 
@@ -53,14 +48,52 @@ def estimate_steps(current, goal):
         return 1
 
 def astarsearch(goalString):
-    visited = []
-
+    bestState = 0
+    visited = {"MI": None}
+    agenda = ["MI"]
+    currentState = agenda[0]
+    lowestCost = estimate_steps(agenda[0], goalString)
+    agendaMaxLen = 1
+    extendCount = 0
+    while agenda:
+        for state in agenda:
+            i=0
+            if lowestCost < estimate_steps(state, goalString):
+                lowestCost = estimate_steps(state, goalString)
+                bestState = i
+            i+=1
+        currentState = agenda.pop(bestState)
+        if currentState == goalString:
+            path = [currentState]
+            while visited[currentState] is not None:
+                currentState = visited[currentState]
+                path.append(currentState)
+            path.reverse()
+            return path, extendCount, agendaMaxLen
+        if extendCount >= 5000:
+            return [0, 0, 0]
+        nextStates = next_states(currentState)
+        for nextState in nextStates:
+            if nextState not in visited:
+                visited[nextState] = currentState
+                agenda.append(nextState)
+        agendaMaxLen = max(agendaMaxLen, len(agenda))
+        extendCount += 1
+    return [0, 0, 0]
 
 currentPath, extendCount, agendaMaxLen = breadthfirstdictionarysearch("MIUUIUUII")
 print(currentPath)
 print(agendaMaxLen)
 print(extendCount)
 
-print(next_states("MIUIU"))
-print(next_states("MIIU"))
-print(next_states("MIIII"))
+
+currentPath, extendCount, agendaMaxLen = astarsearch("MIUUIUUII")
+print(currentPath)
+print(agendaMaxLen)
+print(extendCount)
+
+
+currentPath, extendCount, agendaMaxLen = astarsearch("MUIU")
+print(currentPath)
+print(agendaMaxLen)
+print(extendCount)
